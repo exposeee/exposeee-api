33daf5121284ac672e464ae68536ce879b128e68
joaosr <eng.jmsoares@gmail.com>
2022-05-13 22:18:56 +0200

    Setup channels Create expose consumer and routing

--- core/jobs.py
+++ core/jobs.py
@@ -1,10 +1,17 @@
 import traceback
 from django_rq import job
 from memba_match.entity_handler import EntityHandler
+import channels.layers
+from asgiref.sync import async_to_sync
 
 
 @job('default', timeout=3600)
 def process_expose_file(expose):
+    channel_layer = channels.layers.get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f'exposes_user_{expose.user.id}', {'type': 'chat_message', 'payload': 'New expose processing'}
+    )
+
     try:
         entities = EntityHandler(file_io=expose.file)
 
